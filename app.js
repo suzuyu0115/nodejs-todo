28131446f11f923c71d64bedbcba851c5057e111
suzuyu0115 <szkyut0115@gmail.com>
2024-10-20 19:45:40 +0900

    ルーティングの分割

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
 const express = require("express");
 const app = express();
+const taskRoute = require("./routes/tasks");
+
 const PORT = 8080;
 
-app.get("/api/v1/tasks", (req, res) => {
-  res.send("タスクを全て取得しました");
-});
-
-app.post("/api/v1/tasks", (req, res) => {
-  res.send("タスクを新規作成しました");
-});
-
-app.get("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを取得しました");
-});
-
-app.patch("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを更新しました");
-});
-
-app.delete("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを削除しました");
-});
+app.use("/api/v1/tasks", taskRoute);
 
 app.listen(PORT, console.log("サーバーが起動しました"));
