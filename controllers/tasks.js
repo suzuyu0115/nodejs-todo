5161e0a4387a75423874d359bcb26e084d085e30
suzuyu0115 <szkyut0115@gmail.com>
2024-10-27 00:05:15 +0900

    タスクの全取得と個別取得のロジック作成

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("タスクを全て取得しました");
+const getAllTasks = async (req, res) => {
+  try {
+    const allTask =  await Task.find({});
+    res.status(200).json(allTask);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 };
 
 const createTask = async (req, res) => {
@@ -13,8 +18,16 @@ const createTask = async (req, res) => {
   }
 };
 
-const getSingleTask = (req, res) => {
-  res.send("ある特定のタスクを取得しました");
+const getSingleTask =async (req, res) => {
+  try {
+    const getSingleTask =  await Task.findOne({_id: req.params.id});
+    if (!getSingleTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(getSingleTask);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 };
 
 const updateTask = (req, res) => {
