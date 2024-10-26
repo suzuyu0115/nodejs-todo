f24afd1686b0cc9a4df7a17ee277d1290e898598
suzuyu0115 <szkyut0115@gmail.com>
2024-10-27 01:09:26 +0900

    タスクの削除処理の追加

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -48,8 +48,16 @@ const updateTask = async (req, res) => {
   }
 };
 
-const deleteTask = (req, res) => {
-  res.send("ある特定のタスクを削除しました");
+const deleteTask = async (req, res) => {
+  try {
+    const deleteTask =  await Task.findOneAndDelete({_id: req.params.id});
+    if (!deleteTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(deleteTask);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
