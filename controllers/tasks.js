e68b18f9ea84880a6fcd6f4b71d647406a257c4d
suzuyu0115 <szkyut0115@gmail.com>
2024-10-27 00:40:23 +0900

    タスクの更新処理の追加

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -30,8 +30,22 @@ const getSingleTask =async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("ある特定のタスクを更新しました");
+const updateTask = async (req, res) => {
+  try {
+    const updateTask =  await Task.findOneAndUpdate(
+      {_id: req.params.id},
+      req.body,
+      {
+        new: true,
+      }
+    );
+    if (!updateTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(updateTask);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 };
 
 const deleteTask = (req, res) => {
