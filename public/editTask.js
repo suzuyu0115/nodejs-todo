f96c87177b2d3f6e647bd97b49bc972bd5dfacf9
suzuyu0115 <szkyut0115@gmail.com>
2024-11-06 00:20:58 +0900

    特定のタスクをaxiosで取得

--- public/editTask.js
+++ public/editTask.js
@@ -1,4 +1,21 @@
+const taskIDDOM = document.querySelector(".task-edit-id");
+const taskNameDOM = document.querySelector(".task-edit-name");
+
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
 
 console.log(id);
+
+// 1つの特定のタスクを取得する
+const showTask = async () => {
+  try {
+    const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
+    const { _id, completed, name } = task;
+    taskIDDOM.textContent = _id;
+    taskNameDOM.value = name;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+showTask();
