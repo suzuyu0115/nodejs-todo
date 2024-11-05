20b45cc54f4586569f7fd4008319a125f1f995e8
suzuyu0115 <szkyut0115@gmail.com>
2024-11-06 00:33:40 +0900

    タスク名編集機能の追加

--- public/editTask.js
+++ public/editTask.js
@@ -1,5 +1,7 @@
 const taskIDDOM = document.querySelector(".task-edit-id");
 const taskNameDOM = document.querySelector(".task-edit-name");
+const editFormDOM = document.querySelector(".single-task-form");
+const formAlertDOM = document.querySelector(".form-alert");
 
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
@@ -19,3 +21,25 @@ const showTask = async () => {
 };
 
 showTask();
+
+// タスクの編集
+editFormDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    const taskName = taskNameDOM.value;
+    const { data: task } = await axios.patch(`/api/v1/tasks/${id}`, {
+      name: taskName,
+    });
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "編集に成功しました";
+    formAlertDOM.classList.add("text-success");
+  } catch (err) {
+    console.log(err);
+  }
+
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
